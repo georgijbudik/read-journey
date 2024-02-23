fd05b3336b4eb3c36a43646d89ddba321d7ecfe5
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-23 15:42:42 +0200

    feat: styled recommended list/item

--- app/(main)/_components/recommended-item.tsx
+++ app/(main)/_components/recommended-item.tsx
@@ -1,11 +1,34 @@
+import Image from "next/image";
+
+import { IBook } from "@/types";
+
+import { shortenText } from "@/helpers";
+
 interface IRecommededItemProps {
-  item: string;
+  book: IBook;
 }
 
-const RecommendedItem = ({ item }: IRecommededItemProps) => {
+const RecommendedItem = ({ book }: IRecommededItemProps) => {
+  const { _id, title, author, imageUrl, totalPages, recommend } = book;
+
   return (
-    <div>
-      <p className="text-primary">{item}</p>
+    <div className="flex flex-col items-start gap-2">
+      <div className="rounded-lg w-[137px] h-[208px]">
+        <Image
+          src={imageUrl}
+          alt={title}
+          width={137}
+          height={208}
+          className="w-[137px] h-[208px] rounded-[8px] object-fit"
+        />
+      </div>
+
+      <div>
+        <h4 className="text-stone-50 text-sm font-bold leading-none mb-[2px]">
+          {shortenText(title, 15)}
+        </h4>
+        <p className="text-stone-500 text-xs font-medium leading-3">{author}</p>
+      </div>
     </div>
   );
 };
