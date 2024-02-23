ae7bc7a22512058a9326b1389bdcc8c236ed2e5b
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-23 20:21:49 +0200

    feat: created add book modal

---
+++ app/(main)/library/_components/add-book-modal.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+interface IAddBookModalProps {
+  isValid: boolean;
+  isDirty: boolean;
+}
+
+const AddBookModal = ({ isDirty, isValid }: IAddBookModalProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          className="px-5 md:px-7 py-2.5 md:py-3 rounded-3xl bg-transparent border border-stone-50 border-opacity-20 text-stone-50 text-sm md:text-base font-bold leading-none tracking-tight hover:text-neutral-800 transition-all duration-300"
+          type="submit"
+          disabled={!isValid || !isDirty}
+        >
+          Add book
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[335px] md:max-w-[342px] py-[60px] px-[46px] md:p-[50px]">
+        <div className="flex flex-col items-center justify-center gap-5 md:gap-8">
+          <div className="text-black text-5xl md:text-7xl  font-medium font-['Gilroy'] leading-10">
+            👍
+          </div>
+
+          <div className="flex flex-col gap-2.5 md:gap-3.5">
+            <h4 className="text-center text-stone-50 text-lg md:text-xl font-bold leading-none md:leading-tight">
+              Good job
+            </h4>
+            <p className="text-center text-stone-500 text-sm font-medium leading-none">
+              Your book is now in{" "}
+              <span className="text-stone-50">the library!</span> The joy knows
+              no bounds and now you can start your training
+            </p>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddBookModal;
