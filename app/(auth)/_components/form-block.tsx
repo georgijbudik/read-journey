e1f305d72d4b3929dc6ba39fbdfefcb11ac95148
georgijbudik <georgijbudik@gmail.com>
2024-02-23 14:01:54 +0100

    feat: registration desktop styles

--- app/(auth)/_components/form-block.tsx
+++ app/(auth)/_components/form-block.tsx
@@ -2,11 +2,11 @@ import Logo from "@/components/ui/logo";
 
 const FormBlock = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full bg-foreground rounded-[30px] px-5 pt-5 pb-10 mb-2.5 md:h-full md:pl-10 md:pr-[168px] md:pt-10 md:pb-[214px]">
-      <div className="mb-10 md:mb-[157px]">
+    <div className="w-full bg-foreground rounded-[30px] px-5 pt-5 pb-10 mb-2.5 md:h-full md:pl-10 md:pr-[168px] md:pt-10 md:mb-0 md:pb-[214px] lg:py-10 lg:px-16">
+      <div className="mb-10 md:mb-[157px] lg:mb-[107px]">
         <Logo isAuth={true} />
       </div>
-      <h1 className="text-[32px] text-primary font-bold mb-5 md:text-[64px] md:w-[444px] md:mb-10">
+      <h1 className="text-[32px] text-primary font-bold mb-5 md:text-[64px] md:w-[444px] md:mb-10 leading-none">
         Expand your mind, reading{" "}
         <span className="text-[rgba(227,227,227,0.5)]">a book</span>
       </h1>
--- app/(auth)/_components/image-block.tsx
+++ app/(auth)/_components/image-block.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 
 const ImageBlock = () => {
   return (
-    <div className="w-full bg-foreground rounded-[30px] px-10 pt-5 h-[351px] overflow-hidden md:hidden">
+    <div className="w-full bg-foreground rounded-[30px] px-10 pt-5 h-[351px] overflow-hidden md:hidden lg:block lg:relative lg:h-[720px] lg:object-cover">
       <Image
-        src="/Iphone 15 Black.png"
+        src="/iphone-15-black.png"
         alt="Iphone 15"
         width={255}
         height={518}
+        className="lg:w-[405px] lg:h-[834px] lg:absolute lg:top-[80px] lg:left-1/4"
       />
     </div>
   );
