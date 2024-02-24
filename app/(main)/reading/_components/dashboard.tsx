1f5b1331e3ade268065f7ba61742896ec3c79a13
georgijbudik <georgijbudik@gmail.com>
2024-02-24 15:09:27 +0100

    fix: replaced input

--- app/(main)/reading/_components/dashboard.tsx
+++ app/(main)/reading/_components/dashboard.tsx
@@ -1,102 +1,50 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import Input from "@/components/ui/input";
+import { startReadingSchema } from "@/schemas/startReadingSchema";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { AlertCircle, CheckCircle2 } from "lucide-react";
 import React from "react";
 import { useForm } from "react-hook-form";
-import * as yup from "yup";
 
 interface IStartReading {
   pages: number;
 }
-
-export const schema = yup
-  .object({
-    pages: yup
-      .number()
-      .typeError("Pages must be a number*")
-      .required("Pages are required*"),
-  })
-  .required();
 
 const Dashboard = () => {
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors, touchedFields, isDirty, isValid },
+    formState: { errors, touchedFields },
   } = useForm<IStartReading>({
-    resolver: yupResolver(schema),
+    resolver: yupResolver(startReadingSchema),
     mode: "all",
   });
 
-  const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const onSubmit = async (data: IStartReading) => {
     reset();
   };
 
   return (
-    <form className="md:mr-10">
+    <form className="md:mr-10 md:w-1/2" onSubmit={handleSubmit(onSubmit)}>
       <p className="text-primary text-[10px] mb-2 pl-[14px] md:text-sm">
         Stop page
       </p>
 
-      <div className="relative mb-5" onSubmit={onSubmit}>
-        <input
-          placeholder="0"
-          className={cn(
-            "w-full h-full py-4 pl-[99px] md:pl-[135px] text-primary text-xs md:text-sm md:font-medium relative rounded-xl bg-muted border border-muted outline-0",
-            touchedFields.pages
-              ? errors.pages
-                ? "border border-accent-red"
-                : "border border-accent-green"
-              : ""
-          )}
-          {...register("pages")}
-        />
+      <Input
+        touchedFields={touchedFields}
+        errors={errors}
+        type="pages"
+        heading="Page number"
+        placeholder="0"
+        register={register}
+        padding="pl-[99px] md:pl-[111px]"
+      />
 
-        {touchedFields.pages ? (
-          errors.pages ? (
-            <AlertCircle
-              className={cn(
-                "absolute hidden top-[50%] translate-y-[-50%] right-[14px]",
-                touchedFields.pages
-                  ? errors.pages
-                    ? "block stroke-accent-red"
-                    : "hidden"
-                  : ""
-              )}
-              size={18}
-            />
-          ) : (
-            <CheckCircle2
-              className={cn(
-                "absolute hidden top-[50%] translate-y-[-50%] right-[14px]",
-                touchedFields.pages
-                  ? errors.pages
-                    ? "hidden"
-                    : "block stroke-accent-green"
-                  : ""
-              )}
-              size={18}
-            />
-          )
-        ) : null}
-
-        <p className="text-xs md:text-sm text-secondary absolute top-[50%] translate-y-[-50%] left-[14px] inline-block">
-          Page number:{" "}
-        </p>
-      </div>
-
-      {errors?.pages && touchedFields.pages ? (
-        <p className="text-[10px] text-accent-red mt-1 pl-[14px]">
-          {errors.pages.message}
-        </p>
-      ) : null}
       <Button
         type="submit"
-        className="text-sm mb-10 text-primary bg-transparent py-2.5 border border-[rgba(249,249,249, 0.2)] rounded-[30px] lg:text-base lg:py-3"
+        className="text-sm mb-10 mt-5 text-primary bg-transparent py-2.5 px-5 md:px-7 md:py-3 border border-[rgba(249,249,249, 0.2)] rounded-[30px] md:text-base lg:py-3"
+        variant="outline"
       >
         To start
       </Button>
--- app/(main)/reading/page.tsx
+++ app/(main)/reading/page.tsx
@@ -5,7 +5,7 @@ const ReadingPage = () => {
   return (
     <div>
       <section>
-        <div className="w-full bg-foreground rounded-[30px] pt-5 px-5 pb-10 md:flex lg:flex-col lg:w-[353px]">
+        <div className="w-full bg-foreground rounded-[30px] pt-5 px-5 pb-10 md:flex md:pt-8 md:pl-8 md:pb-4 lg:flex-col lg:w-[353px]">
           <Dashboard />
           <Diary />
         </div>
