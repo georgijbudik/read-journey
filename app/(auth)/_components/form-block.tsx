2f360b654f3cca595134f2fd8d7f79dff7c9bc95
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-24 13:26:02 +0200

    fix: auth reusable blocks

--- app/(auth)/_components/form-block.tsx
+++ app/(auth)/_components/form-block.tsx
@@ -2,16 +2,18 @@ import Logo from "@/components/ui/logo";
 
 const FormBlock = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full bg-foreground rounded-[30px] px-5 pt-5 pb-10 mb-2.5 md:h-full md:pl-10 md:pr-[168px] md:pt-10 md:mb-0 md:pb-[214px] lg:py-10 lg:px-16">
-      <div className="mb-10 md:mb-[157px] lg:mb-[107px]">
-        <Logo isAuth={true} />
+    <section className="w-full md:flex-grow lg:flex-grow-0 bg-foreground rounded-3xl px-5 pt-5 pb-10 md:pt-10 md:px-[70px] lg:px-[64px]">
+      <div className="mb-10 md:mb-[157px] lg:mb-[107px] flex flex-start">
+        <Logo isAuth />
       </div>
-      <h1 className="text-[32px] text-primary font-bold mb-5 md:text-[64px] md:w-[444px] md:mb-10 leading-none">
+
+      <h1 className="md:w-[444px] text-stone-50 text-3xl md:text-6xl font-bold tracking-wide md:tracking-wider mb-5 md:mb-10">
         Expand your mind, reading{" "}
-        <span className="text-[rgba(227,227,227,0.5)]">a book</span>
+        <span className="text-neutral-200 text-opacity-50">a book</span>
       </h1>
+
       {children}
-    </div>
+    </section>
   );
 };
 
