56fe797bb48a31c968401f19f45f822aa9578626
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-24 14:50:52 +0200

    feat: created reusable input

---
+++ components/ui/input.tsx
@@ -0,0 +1,88 @@
+import { cn } from "@/lib/utils";
+import { AlertCircle, CheckCircle2 } from "lucide-react";
+
+interface IInputProps {
+  touchedFields: any;
+  errors: any;
+  register: any;
+  heading: string;
+  type: string;
+  placeholder: string;
+  inputType?: string;
+  padding: string;
+  children?: React.ReactNode;
+}
+
+const Input = ({
+  touchedFields,
+  errors,
+  register,
+  type,
+  placeholder,
+  heading,
+  inputType = "text",
+  padding,
+  children,
+}: IInputProps) => {
+  return (
+    <div>
+      <div className="relative">
+        <input
+          type={inputType}
+          placeholder={placeholder}
+          className={cn(
+            `w-full h-full py-[14px] md:py-4 ${padding} text-primary text-xs md:text-sm md:font-medium relative rounded-xl bg-muted border border-muted outline-0`,
+            touchedFields[type]
+              ? errors[type]
+                ? "border border-accent-red"
+                : "border border-accent-green"
+              : ""
+          )}
+          {...register(type)}
+        />
+
+        {children}
+
+        {touchedFields[type] ? (
+          errors[type] ? (
+            <AlertCircle
+              className={cn(
+                "absolute hidden top-[50%] translate-y-[-50%] right-[14px]",
+                touchedFields[type]
+                  ? errors[type]
+                    ? "block stroke-accent-red"
+                    : "hidden"
+                  : ""
+              )}
+              size={18}
+            />
+          ) : (
+            <CheckCircle2
+              className={cn(
+                "absolute hidden top-[50%] translate-y-[-50%] right-[14px]",
+                touchedFields[type]
+                  ? errors[type]
+                    ? "hidden"
+                    : "block stroke-accent-green"
+                  : ""
+              )}
+              size={18}
+            />
+          )
+        ) : null}
+
+        <p className="text-xs md:text-sm text-secondary absolute top-[50%] translate-y-[-50%] left-[14px] inline-block">
+          {heading}:{" "}
+        </p>
+      </div>
+
+      {errors[type] && touchedFields[type] ? (
+        <p className="text-[10px] text-accent-red mt-1 pl-[14px]">
+          {errors[type].message}
+        </p>
+      ) : null}
+    </div>
+  );
+};
+
+export default Input;
