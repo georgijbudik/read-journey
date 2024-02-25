9ddb63832e77c367664caeb9204de5ac90b5dcbf
georgijbudik <georgijbudik@gmail.com>
2024-02-25 12:01:49 +0100

    fix: shemas

--- schemas/registrationSchema.ts
+++ schemas/registrationSchema.ts
@@ -3,10 +3,10 @@ import * as yup from "yup";
 export const registrationSchema = yup
   .object({
     name: yup.string().required("Name is required*"),
-    mail: yup
+    email: yup
       .string()
-      .required("Mail is required*")
-      .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Invalid mail format*"),
+      .required("Email is required*")
+      .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Invalid email format*"),
     password: yup
       .string()
       .required("Password is required*")
