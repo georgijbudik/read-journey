bfc118c88a0ce021b63c3e041fc37d6ce8f647cb
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-26 20:20:17 +0200

    fix: styles rounding

--- app/(main)/library/page.tsx
+++ app/(main)/library/page.tsx
@@ -15,7 +15,7 @@ const LibraryPage = ({
 
   return (
     <div className="flex flex-col lg:flex-row gap-2.5 md:gap-4">
-      <section className="bg-foreground rounded-3xl p-5 md:p-8 flex flex-col md:flex-row lg:flex-col gap-5 md:gap-8">
+      <section className="bg-foreground rounded-[30px] p-5 md:p-8 flex flex-col md:flex-row lg:flex-col gap-5 md:gap-8">
         <div className="md:flex-grow-[3]">
           <AddBookForm />
         </div>
