1a3c7f555be8d0c293f215f96dfd5b8671ea32ce
georgijbudik <georgijbudik@gmail.com>
2024-02-26 11:17:30 +0100

    add pagination

--- app/(main)/_components/pagination.tsx
+++ app/(main)/_components/pagination.tsx
@@ -1,9 +1,28 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-const Pagination = () => {
+const Pagination = ({ page }: { page: number }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const addPageToUrl = (pageNumber: number | null) => {
+    if (!pageNumber || pageNumber <= 0) {
+      return pathname;
+    }
+
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <div className="flex items-center gap-2">
-      <button className="group w-8 h-8 md:h-10 md:w-10 rounded-full border border-stone-50 border-opacity-20 flex items-center justify-center hover:border-opacity-100 transition-all duration-300">
+      <button
+        className="group w-8 h-8 md:h-10 md:w-10 rounded-full border border-stone-50 border-opacity-20 flex items-center justify-center hover:border-opacity-100 transition-all duration-300"
+        onClick={() => addPageToUrl(page - 1)}
+      >
         <svg
           className={cn(
             "h-4 w-4 md:w-5 md:h-5 fill-none stroke-primary/20 group-hover:stroke-primary/100 transition-all duration-300"
@@ -12,7 +31,10 @@ const Pagination = () => {
           <use xlinkHref="/sprite.svg#icon-left"></use>
         </svg>
       </button>
-      <button className="group w-8 h-8 md:h-10 md:w-10 rounded-full border border-stone-50 border-opacity-20 flex items-center justify-center hover:border-opacity-100 transition-all duration-300">
+      <button
+        className="group w-8 h-8 md:h-10 md:w-10 rounded-full border border-stone-50 border-opacity-20 flex items-center justify-center hover:border-opacity-100 transition-all duration-300"
+        onClick={() => addPageToUrl(page - 1)}
+      >
         <svg
           className={cn(
             "h-4 w-4 md:w-5 md:h-5 fill-none stroke-primary/20 group-hover:stroke-primary/100 transition-all duration-300"
--- app/(main)/_components/recommended-section.tsx
+++ app/(main)/_components/recommended-section.tsx
@@ -1,16 +1,26 @@
+import { getBooks } from "@/app/api/data";
 import Pagination from "./pagination";
 import RecommendedList from "./recommended-list";
 
-const RecommendedSection = () => {
+const RecommendedSection = async ({
+  searchParams,
+}: {
+  searchParams: {
+    page?: string;
+  };
+}) => {
+  const page = Number(searchParams?.page) || 1;
+  const books = await getBooks({ limit: 2, page });
+
   return (
     <div className="py-[40px] px-[20px] md:px-[40px] bg-foreground rounded-3xl">
       <div className="flex justify-between items-start mb-[22px] md:mb-[20px]">
         <h3 className="text-stone-50 text-xl md:text-3xl font-bold leading-tight ">
           Recommended
         </h3>
-        <Pagination />
+        <Pagination page={page} />
       </div>
-      <RecommendedList />
+      <RecommendedList books={books} />
     </div>
   );
 };
