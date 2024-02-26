b14f7e069918e792ae3cbcc4a35134f5ac287965
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-26 20:22:03 +0200

    fix: quote

--- app/(main)/_components/dashboard-quote.tsx
+++ app/(main)/_components/dashboard-quote.tsx
@@ -3,8 +3,8 @@ const DashboardQuote = () => {
     <div className="flex items-center gap-[14px] bg-neutral-800 rounded-xl py-[15px] px-5">
       <p className="text-black text-4xl font-medium leading-10">📚</p>
       <p className="max-w-[197px] text-stone-500 text-sm font-medium leading-none">
-        "Books are <span className="text-primary">windows</span> to the world,
-        and reading is a journey into the unknown."
+        &quot;Books are <span className="text-primary">windows</span> to the
+        world, and reading is a journey into the unknown.&quot;
       </p>
     </div>
   );
