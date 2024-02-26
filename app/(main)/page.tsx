e2811ca91fd9a558cceab331b30847cf68eaf603
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-26 21:53:41 +0200

    fix: added desktop fixed width

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -19,7 +19,7 @@ const RecommendedPage = ({
   return (
     <div className="flex flex-col lg:flex-row gap-2.5 md:gap-4">
       <section className="bg-foreground rounded-[30px] p-5 md:p-8 flex flex-col md:flex-row lg:flex-col gap-5 md:gap-8">
-        <div className="md:flex-grow-[3]">
+        <div className="md:flex-grow-[3] lg:min-w-[330px]">
           <FilterForm />
         </div>
 
