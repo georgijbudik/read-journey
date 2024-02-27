f67ec0ca0d72ed3b636b2ea0fba150a3fb77510a
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-27 18:15:37 +0200

    feat: created user page

---
+++ app/(main)/profile/_components/user-history.tsx
@@ -0,0 +1,15 @@
+const UserHistory = () => {
+  return (
+    <section className="lg:flex-grow bg-foreground rounded-[30px] py-[40px] px-[20px] md:px-[40px]">
+      <div className="flex justify-between items-start mb-[14px]">
+        <h3 className="text-stone-50 text-xl md:text-3xl font-bold leading-tight md:tracking-wide">
+          My history
+        </h3>
+
+        {/* <LibrarySelect /> */}
+      </div>
+    </section>
+  );
+};
+
+export default UserHistory;
