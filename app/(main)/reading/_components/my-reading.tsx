cca5e5ed3d6c0e968ebb579ea788194cd95c44dd
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-29 14:35:53 +0200

    feat: created readble book

--- app/(main)/reading/_components/my-reading.tsx
+++ app/(main)/reading/_components/my-reading.tsx
@@ -1,37 +1,38 @@
 "use client";
 
-import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+
+import Book from "./book";
 
 import { getBookById } from "@/app/api/book-actions";
-import { useEffect, useState } from "react";
+
 import { Userbook } from "@prisma/client";
-import { useSession } from "next-auth/react";
 
 const MyReading = ({ bookId }: { bookId: string }) => {
   const [book, setBook] = useState<Userbook | null>(null);
+
   const { data } = useSession();
   const email = data?.user?.email as string;
+
+  useEffect(() => {
+    fetchBookById();
+  }, []);
+
   const fetchBookById = async () => {
     const book = await getBookById({ email, bookId });
     setBook(book);
   };
-  useEffect(() => {
-    fetchBookById();
-  }, []);
+
   return (
     <section className="lg:w-full lg:h-full">
       <div className="bg-foreground py-10 px-5 mt-2.5 lg:mt-0 rounded-[30px] md:px-10 md:pb-[25px] lg:pb-[53px]">
         <h3 className="text-primary text-xl font-bold mb-10 md:text-[28px] md:mb-8 lg:mb-[44px]">
           My library
         </h3>
-        <div className="flex flex-col items-center">
-          <Image
-            src={(book?.imageUrl as string) || ""}
-            alt={(book?.title as string) || ""}
-            width={224}
-            height={340}
-            className="w-[137px] h-[208px] rounded-[8px] mb-[10px] md:w-[169px] md:h-[256px] md:mb-[25px] lg:w-[224px] lg:h-[340px] "
-          />
+        <Book bookCoverUrl={book?.imageUrl as string} />
+
+        <div className="flex flex-col items-center justify-center">
           <h4 className="text-primary text-sm font-bold mb-[5px] md:text-xl">
             {book?.title}
           </h4>
